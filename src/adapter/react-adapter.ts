6b05e9fcf092dfe340e356b72103fb9a456deb0e
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-10 00:30:02 +0600

    Working on UI

--- src/adapter/react-adapter.ts
+++ src/adapter/react-adapter.ts
@@ -3,6 +3,7 @@ import * as React from "react"
 export type mmReactNode = React.ReactNode
 export type mmReactComponentType<P> = React.ComponentType<P>
 export type mmReactComponentProps<P extends React.ElementType> = React.ComponentProps<P>;
+export type mmReactElement = React.ElementType
 
 export const mmReactUseState = React.useState
 export const mmReactUseRef = React.useRef
--- src/data/mm-data-type.ts
+++ src/data/mm-data-type.ts
@@ -1,5 +1,7 @@
-import {mmReactComponentType, mmReactNode} from "./../adapter/react-adapter";
+import {mmReactComponentProps, mmReactComponentType, mmReactElement, mmReactNode} from "./../adapter/react-adapter";
 
 
+export type UIElement = mmReactElement
 export type UINode = mmReactNode
 export type UIComponent<P> = mmReactComponentType<P>
+export type UIComponentProps<P extends UIElement> = mmReactComponentProps<P>
