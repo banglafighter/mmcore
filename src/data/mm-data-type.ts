4079f4de3f3a49f9687d6d507efc3a25dd1ad116
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-02 19:16:58 +0600

    Added Type

--- src/data/mm-data-type.ts
+++ src/data/mm-data-type.ts
@@ -5,3 +5,4 @@ export type UIElement = mmReactElement
 export type UINode = mmReactNode
 export type UIComponent<P> = mmReactComponentType<P>
 export type UIComponentProps<P extends UIElement> = mmReactComponentProps<P>
+export type MixType = string | boolean | number
