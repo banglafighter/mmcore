83fab18f310e5cd6b906c3ded25cc7aff84692e9
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-06-18 01:45:09 +0600

    updating various things

--- src/adapter/react-adapter.ts
+++ src/adapter/react-adapter.ts
@@ -3,18 +3,16 @@ import * as React from "react"
 export type mmReactNode = React.ReactNode
 export type mmReactComponentType<P> = React.ComponentType<P>
 export type mmReactComponentProps<P extends React.ElementType> = React.ComponentProps<P>;
-export type mmReactElement = React.ElementType
+export type mmReactElementType = React.ElementType
 
 export const mmReactUseState = React.useState
 export const mmReactUseMemo = React.useMemo
 export const mmReactUseCallback = React.useCallback
-export const mmReactUseEffect = React.useEffect
 export const mmReactUseContext = React.useContext
 export const MmReactFragment = React.Fragment
 
 export const mmReactUseRef = React.useRef
 
-export const mmReactCreateContext = React.createContext
 export const mmReactLazy = React.lazy
 
 export const MmReactSuspense = React.Suspense
@@ -29,4 +27,9 @@ export type {
 export {
     forwardRef as mmReactForwardRef,
     useImperativeHandle as mmReactUseImperativeHandle,
+    cloneElement as mmReactCloneElement,
+    ReactElement as mmReactElement,
+    createRef as mmReactCreateRef,
+    createContext as mmReactCreateContext,
+    useEffect as mmReactUseEffect
 } from "react"
--- src/data/mm-data-type.ts
+++ src/data/mm-data-type.ts
@@ -1,8 +1,15 @@
-import {mmReactComponentProps, mmReactComponentType, mmReactElement, mmReactNode} from "./../adapter/react-adapter";
+import {
+    mmReactComponentProps,
+    mmReactComponentType,
+    mmReactElement,
+    mmReactElementType,
+    mmReactNode
+} from "./../adapter/react-adapter";
 
 
 export type UIElement = mmReactElement
+export type UIElementType = mmReactElementType
 export type UINode = mmReactNode
 export type UIComponent<P> = mmReactComponentType<P>
-export type UIComponentProps<P extends UIElement> = mmReactComponentProps<P>
+export type UIComponentProps<P extends UIElementType> = mmReactComponentProps<P>
 export type MixType = string | boolean | number
